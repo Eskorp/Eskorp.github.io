4b13e6f90e8e179c1e9e260297ee0bc403f8609b
Sergey <pund250298@gmail.com>
2018-05-07 15:13:39 +0300

    task_2
    Cesax

---
+++ skate/Cesax_test/js/list.js
@@ -0,0 +1,51 @@
+
+
+$(".catalog_list_item1").click(function(){
+    if ($(".catalog_underlist1").is(":hidden")) {      
+   $(".catalog_underlist1").slideDown("fast");
+   } else {
+      $(".catalog_underlist1").slideUp("fast");
+}
+})
+
+$(".catalog_list_item2").click(function(){
+     if ($(".catalog_underlist2").is(":hidden")) {   
+   $(".catalog_underlist2").slideDown("fast");
+   } else {
+      $(".catalog_underlist2").slideUp("fast");
+}
+})
+
+$(".catalog_list_item3").click(function(){
+     if ($(".catalog_underlist3").is(":hidden")) {   
+   $(".catalog_underlist3").slideDown("fast");
+   } else {
+      $(".catalog_underlist3").slideUp("fast");
+}
+})
+
+$(".catalog_list_item4").click(function(){
+      if ($(".catalog_underlist4").is(":hidden")) {   
+   $(".catalog_underlist4").slideDown("fast");
+   } else {
+      $(".catalog_underlist4").slideUp("fast");
+}
+})
+
+$(".catalog_list_item5").click(function(){
+      if ($(".catalog_underlist5").is(":hidden")) {   
+   $(".catalog_underlist5").slideDown("fast");
+   } else {
+      $(".catalog_underlist5").slideUp("fast");
+}
+})
+
+$(".catalog_list_item6").click(function(){
+      if ($(".catalog_underlist6").is(":hidden")) {   
+   $(".catalog_underlist6").slideDown("fast");
+   } else {
+      $(".catalog_underlist6").slideUp("fast");
+}
+})
+
+
---
+++ skate/Cesax_test/js/slider.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+  
+  infinite: true,
+  speed: 500,
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  cssEase: 'linear'
+});
+   
